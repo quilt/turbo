c9939d7fd815a5848308ba8ee7aacc50e8c8befb
Sam Wilson <sam.wilson@mesh.xyz>
2021-01-26 15:25:12 -0500

    Add README; use config file

--- turbo-txpool/src/bin/turbo-txpool.rs
+++ turbo-txpool/src/bin/turbo-txpool.rs
@@ -1,17 +1,49 @@
-use std::net::Ipv4Addr;
+use serde::{Deserialize, Serialize};
+
+use snafu::{ResultExt, Snafu};
+
+use std::fs;
+use std::net::SocketAddr;
+use std::path::PathBuf;
+
+use structopt::StructOpt;
 
 use turbo_txpool::config::Config;
 use turbo_txpool::TxPool;
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), tonic::transport::Error> {
-    let config = Config::builder()
-        .control("http://127.0.0.1:9092") // TODO
-        .max_txs(1024)
-        .build();
+#[derive(Debug, Serialize, Deserialize)]
+struct TonicConfig {
+    bind: SocketAddr,
+    #[serde(flatten)]
+    config: Config,
+}
 
-    TxPool::with_config(config)
-        .await?
-        .run((Ipv4Addr::LOCALHOST, 54001))
+#[derive(Debug, Snafu)]
+enum Error {
+    Tonic { source: tonic::transport::Error },
+    Io { source: std::io::Error },
+    Toml { source: toml::de::Error },
+}
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(short = "c", help = "Path to TOML configuration file")]
+    config: PathBuf,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Error> {
+    let opt = Opt::from_args();
+    let config_bytes = fs::read(&opt.config).context(Io)?;
+    let tonic_config: TonicConfig =
+        toml::de::from_slice(&config_bytes).context(Toml)?;
+
+    TxPool::with_config(tonic_config.config)
         .await
+        .context(Tonic)?
+        .run(tonic_config.bind)
+        .await
+        .context(Tonic)?;
+
+    Ok(())
 }
